31bee725b8a15dcab9c9b447ce99b227cd8fd065
Deddryk <Deddryk@gmail.com>
2018-03-12 21:39:59 -0400

    Adding pokemon module

--- dedbot/pokemon.py
+++ dedbot/pokemon.py
@@ -1,9 +1,10 @@
 import asyncio
+import re
 
 import discord
 
 POKECORD_ID = '365975655608745985'
-POKEMON_REGEX = '[0-9]{3}(.+)(-.+)?\.png'
+POKEMON_REGEX = '[0-9]{3}([A-Z][a-z]+)(-.+)?\.png(/\.*)?'
 
 class PokeCatcher():
 
@@ -18,17 +19,17 @@ class PokeCatcher():
         if not message.embeds:
             return False
         embed = message.embeds[0]
-        if 'title' in embed.keys() and embed['title'].startswith("A wile po"):
+        if 'title' in embed.keys() and embed['title'].startswith("A wild po"):
             return True
         
     def get_wild_pokemon(message):
         url = message.embeds[0]['image']['url']
-        return re.search(POKEMON_REGEX, url]).group(1)
+        return re.search(POKEMON_REGEX, url).group(1)
 
     async def on_message(self, message):
-        if self.message_matters(message) and is_wild_pokemon(message):
+        if self.message_matters(message) and PokeCatcher.is_wild_pokemon(message):
             if self.channels[message.channel]:
-                await self.client.message.send(message.channel, 'p!catch ' + get_wild_pokemon(message))
+                await self.client.send_message(message.channel, 'p!catch ' + PokeCatcher.get_wild_pokemon(message))
 
     def toggle_autocatch(self, channel):
         if channel in self.channels.keys():
