fca7440ba44967732a2ee37d328e5ea80ead9a4d
HopeAnnihilator <36973966+HopeAnnihilator@users.noreply.github.com>
2018-03-16 15:33:57 -0500

    Update pokemon.py

--- dedbot/pokemon.py
+++ dedbot/pokemon.py
@@ -29,14 +29,14 @@ class PokeCatcher():
         return re.search(POKEMON_REGEX, url).group(1)
 
     async def on_message(self, message):
-        if self.message_matters(message) and PokeCatcher.is_wild_pokemon(message) and (self.catch_all or PokeCatcher.get_wild_pokemon(message) in self.catch_list):
+        if self.message_matters(message) and PokeCatcher.is_wild_pokemon(message) and (self.catch_all or PokeCatcher.get_wild_pokemon(message) in self.catch_list or PokeCatcher.get_wild_pokemon(message) not in self.caught_file):
             if self.channels[message.channel]:
                 await self.client.send_message(message.channel, 'p!catch ' + PokeCatcher.get_wild_pokemon(message))
                 if PokeCatcher.get_wild_pokemon(message) in self.catch_list:
                     with open('files/spotted_filter.txt', 'a' ) as spotted:
-                    spotted.write('Spotted: ' + PokeCatcher.get_wild_pokemon(message) + '\n')
+                        spotted.write('Spotted: ' + PokeCatcher.get_wild_pokemon(message) + '\n')
                 with open('files/spotted_all.txt', 'a' ) as spotted:
-                spotted.write('Spotted: ' + PokeCatcher.get_wild_pokemon(message) + '\n')
+                    spotted.write('Spotted: ' + PokeCatcher.get_wild_pokemon(message) + '\n')
 
     def toggle_autocatch(self, channel):
         if channel in self.channels.keys():
@@ -44,4 +44,12 @@ class PokeCatcher():
         else:
             self.channels[channel] = True
             
+         
         
+    def is_caught(message):
+        if message_matters(message) and client.user.name in message and 'aug' in message:
+            hard_shit = message.split()
+            caught_pokemon = hard_shit.split('1')
+               with open(self.caught_file, 'a' ) as caught:
+                   caught.write(caught_pokemon + '\n')
+            
